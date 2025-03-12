675c0a1552d9f16977b7517a10c99f77c2a1b316
TianQize <3063400699@qq.com>
2025-03-12 11:24:53 +0800

    update

---
+++ stream-game-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="text-red-500">项目初始化成功</div>
+      <div className="text-red-500">测试 pr</div>
+      <div className="text-red-500">我是 yqj</div>
+    </div>
+  );
+}
