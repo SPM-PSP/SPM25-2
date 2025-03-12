11d4066b0964860041032a5b6d608ad247704875
zhangbihan999 <zhangbihan999@gmail.com>
2025-03-12 10:59:28 +0800

    yqj

--- stream-game-app/src/app/page.tsx
+++ stream-game-app/src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div>
       <div className="text-red-500">项目初始化成功</div>
       <div className="text-red-500">测试 pr</div>
+      <div className="text-red-500">我是 yqj</div>
     </div>
   );
 }
