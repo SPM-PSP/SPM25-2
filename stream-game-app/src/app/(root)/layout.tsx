783187e7c68703cfa85af5c9735c0617897e1ce9
zhangbihan999 <zhangbihan999@gmail.com>
2025-04-01 23:57:11 +0800

    添加了背景图片

--- stream-game-app/src/app/(root)/layout.tsx
+++ stream-game-app/src/app/(root)/layout.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Image from "next/image";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <h1 className="text-3xl">NavBar</h1>
-      {children}
+      {/* 背景图片 */}
+      <Image
+        src="/root_background.jpg"
+        alt="Background"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        priority={true}
+      />
+      {/* 前景内容 */}
+      <div style={{ position: "relative", zIndex: 1 }}>
+        <h1 className="text-3xl">NavBar</h1>
+        {children}
+      </div>
     </div>
   );
 };
