72b9a9529567e4d722c044c4f5c422180b9cd396
RSCML <454020090@qq.com>
2025-04-02 09:38:34 +0800

    Revert "Merge branch 'main' into wjh"
    This reverts commit 6f5192a162e7ea818307b5f0565e96a3b8982230, reversing
    changes made to d6213183643f835c19ecf4933c4a906c2639e7b4.

--- stream-game-app/src/app/(root)/layout.tsx
+++ stream-game-app/src/app/(root)/layout.tsx
@@ -1,23 +1,10 @@
 import React from "react";
-import Image from "next/image";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      {/* 背景图片 */}
-      <Image
-        src="/root_background.jpg"
-        alt="Background"
-        layout="fill"
-        objectFit="cover"
-        quality={100}
-        priority={true}
-      />
-      {/* 前景内容 */}
-      <div style={{ position: "relative", zIndex: 1 }}>
-        <h1 className="text-3xl">NavBar</h1>
-        {children}
-      </div>
+      <h1 className="text-3xl">NavBar</h1>
+      {children}
     </div>
   );
 };
