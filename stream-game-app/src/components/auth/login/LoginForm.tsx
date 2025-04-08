323a3402fe5f606bc9a6bbeadf38321d8a92b220
zhangbihan999 <zhangbihan999@gmail.com>
2025-04-08 22:55:07 +0800

    完成了登录功能

--- stream-game-app/src/components/auth/login/LoginForm.tsx
+++ stream-game-app/src/components/auth/login/LoginForm.tsx
@@ -1,14 +1,47 @@
-import React from "react";
-import Form from "next/Form";
+"use client";
+import React, { useState } from "react";
+import supabase from "@/lib/supabase";
+import { useRouter } from "next/navigation";
 
 const LoginForm = () => {
+  const router = useRouter();
+
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.target as HTMLFormElement;
+    const email = form.elements.namedItem("email") as HTMLInputElement;
+    const password = form.elements.namedItem("password") as HTMLInputElement;
+
+    // 从数据库中查询是否存在该用户名
+    const { data, error } = await supabase
+      .from("user")
+      .select("*")
+      .eq("email", email.value)
+      .single();
+
+    if (error || !data) {
+      console.log("error: ", error);
+      alert("账户不存在");
+      return;
+    }
+
+    // 检查密码是否匹配
+    if (data.password !== password.value) {
+      alert("密码错误，请重新输入");
+      return;
+    }
+
+    router.push("/dashboard");
+  };
+
   return (
     <div className="fixed max-w-[450px] w-full h-full flex items-center justify-center">
       <div className="bg-white/50  backdrop-blur-xs p-8 rounded-[15px] shadow-[0_0_25px_rgba(255,255,255,0.6)] w-full">
-        <Form action="">
+        <form onSubmit={handleLogin}>
           <div className="mb-2 flex justify-between">
-            <label htmlFor="username" className="text-l">
-              用户名
+            <label htmlFor="email" className="text-l text-black">
+              邮箱
             </label>
             <a
               href="/register"
@@ -19,25 +52,30 @@ const LoginForm = () => {
           </div>
           <input
             type="text"
-            id="username"
-            placeholder="请输入用户名"
-            className="w-full p-2 border border-gray-100 bg-white opacity-75 h-auto rounded-lg mb-4 focus:outline-none focus:border-blue-500"
+            name="email"
+            placeholder="请输入邮箱"
+            className="text-black w-full p-2 border border-gray-100 bg-white opacity-75 h-auto rounded-lg mb-4 focus:outline-none focus:border-blue-500"
+            required
           />
           <div className="mb-2 flex justify-between">
-            <label htmlFor="password" className="text-l">
+            <label htmlFor="password" className="text-l text-black">
               密码
             </label>
           </div>
           <input
             type="password"
-            id="password"
+            name="password"
             placeholder="请输入密码"
-            className="w-full p-2 border border-gray-100 bg-white opacity-75 h-auto rounded-lg mb-4 focus:outline-none focus:border-blue-500"
+            className="text-black w-full p-2 border border-gray-100 bg-white opacity-75 h-auto rounded-lg mb-4 focus:outline-none focus:border-blue-500"
+            required
           />
-          <button className="mt-5 w-full p-2 bg-pink-500 text-white rounded-lg hover:bg-pink-600">
+          <button
+            type="submit"
+            className="mt-5 w-full p-2 bg-pink-500 text-white rounded-lg hover:bg-pink-600"
+          >
             登 录
           </button>
-        </Form>
+        </form>
       </div>
     </div>
   );
