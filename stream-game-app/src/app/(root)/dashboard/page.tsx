4d1356ba3db4898ab595644fd58d7c012ba2134f
RSCML <454020090@qq.com>
2025-04-16 11:40:14 +0800

    Update page.tsx

--- stream-game-app/src/app/(root)/dashboard/page.tsx
+++ stream-game-app/src/app/(root)/dashboard/page.tsx
@@ -102,26 +102,6 @@ export default function Home() {
           </div>
         ))}
       </div>
-      {/* 游戏推荐表格 */}
-      <div>
-        <h1 className="text-white">（占位）表格</h1>
-      </div>
-      <h1 className="text-white">dashboard</h1>
-
-      <ul className="mt-10 text-white">
-        <li>
-          <Link href="/gameDetail/1">game 1</Link>
-        </li>
-        <li>
-          <Link href="/gameDetail/2">game 2</Link>
-        </li>
-        <li>
-          <Link href="/gameDetail/3">game 3</Link>
-        </li>
-        <li>
-          <Link href="/gameDetail/4">game 4</Link>
-        </li>
-      </ul>
     </div>
   );
 }
