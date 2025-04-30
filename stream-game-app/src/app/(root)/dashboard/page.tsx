5541df332967b37efe4b60ff4ea71a6d6305d0c4
zhangbihan999 <zhangbihan999@gmail.com>
2025-04-30 10:06:48 +0800

    添加了测试设计文档

--- stream-game-app/src/app/(root)/dashboard/page.tsx
+++ stream-game-app/src/app/(root)/dashboard/page.tsx
@@ -1,17 +1,15 @@
 "use client";
-import Carousel from "@/components/dashboard/carousel/carousel" ;
+import Carousel from "@/components/dashboard/carousel/carousel";
 import GameTable from "@/components/dashboard/gametable/gametable";
 
 export default function Home() {
   return (
     <div>
-      <div className="flex container items-center justify-center pt-16 pb-0">
-        <Carousel>
-        </Carousel>
+      <div className="flex container items-center justify-center pb-0">
+        <Carousel></Carousel>
       </div>
       <div className="pt-4 pb-20">
-        <GameTable>
-        </GameTable>
+        <GameTable></GameTable>
       </div>
     </div>
   );
